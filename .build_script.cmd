09fcd3f92ac3d4f75aeb56a886037a2617b1c9c2
Mark P Thomas <markums@gmail.com>
2020-05-21 13:16:46 -0600

    Update .build_script.cmd

--- .build_script.cmd
+++ .build_script.cmd
@@ -1,3 +1,3 @@
-nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory ./Math/tools
-nuget install OpenCover -Version 4.6.519 -OutputDirectory ./Math/tools
-nuget install coveralls.net -Version 0.412.0 -OutputDirectory ./Math/tools
+nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory ./tools
+nuget install OpenCover -Version 4.6.519 -OutputDirectory ./tools
+nuget install coveralls.net -Version 0.412.0 -OutputDirectory ./tools
