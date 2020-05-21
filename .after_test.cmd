4a69306b1f2eecc0ec44a514281cf910646d1a8b
Mark P Thomas <markums@gmail.com>
2020-05-21 17:19:47 -0600

    Update .after_test.cmd

--- .after_test.cmd
+++ .after_test.cmd
@@ -14,9 +14,9 @@ nuget install OpenCover -OutputDirectory packages -Version 4.6.519
 .\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:dotnet.exe -targetargs:"test "".\MPT.Math.UnitTests\MPT.Math.UnitTests.csproj"" " -filter:"+[MPT.Math*]*" -oldStyle -output:opencoverCoverage.xml
 
 
-dotnet tool install coveralls.net --version 1.0.0-beta0002 --tool-path tools
+#dotnet tool install coveralls.net --version 1.0.0-beta0002 --tool-path tools
 
-.\tools\csmacnz.coveralls.exe --opencover -i opencoverCoverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
+#.\tools\csmacnz.coveralls.exe --opencover -i opencoverCoverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
 
 #.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:dotnet.exe "-targetargs:"".\packages\xunit.runner.console.2.4.1\tools\netcoreapp2.0\xunit.console.dll"" "".\MPT.Math.xUnitTests\MPT.Math.xUnitTests.csproj"" -noshadow -appveyor" -filter:"+[MPT.Math*]*" -oldStyle -output:opencoverCoverage.xml
 
