6f0dece6d07e436384fdb3fad2361cf992b13cf2
Mark P Thomas <markums@gmail.com>
2020-05-21 14:35:56 -0600

    Update .after_test.cmd

--- .after_test.cmd
+++ .after_test.cmd
@@ -1,3 +1,3 @@
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:".\tools\NUnit.ConsoleRunner.3.11.1\tools\nunit3-console.exe" -targetargs:"/nologo /noshadow .\MPT.Math.UnitTests\bin\Debug\netcoreapp2.1\MPT.Math.UnitTests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user  -filter:"+[*]*" -target:".\tools\NUnit.ConsoleRunner.3.11.1\tools\nunit3-console.exe" -targetargs:".\MPT.Math.UnitTests\bin\Debug\netcoreapp2.1\MPT.Math.UnitTests.dll"
  
 .\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
