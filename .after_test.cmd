4e8842eebfb71f7ca4912f7fe22036e7f13305e7
Mark P Thomas <markums@gmail.com>
2020-05-21 17:17:59 -0600

    Travis & AppVeyor setup  - Testing

--- .after_test.cmd
+++ .after_test.cmd
@@ -10,6 +10,7 @@ nuget install OpenCover -OutputDirectory packages -Version 4.6.519
 
 	
 # nUnit	
+#nuget install NUnit.Runners -Version 3.11.1 -OutputDirectory packages
 .\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:dotnet.exe -targetargs:"test "".\MPT.Math.UnitTests\MPT.Math.UnitTests.csproj"" " -filter:"+[MPT.Math*]*" -oldStyle -output:opencoverCoverage.xml
 
 
