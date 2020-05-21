a12e6fa08b228aa99e9178885574c64cf2e5c1f9
Mark P Thomas <markums@gmail.com>
2020-05-21 11:42:13 -0600

    Travis & AppVeyor setup

--- .after_test.cmd
+++ .after_test.cmd
@@ -1,3 +1,3 @@
-.\Math\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:".\Math\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe" -targetargs:"/nologo /noshadow .\Math\MPT.Math.UnitTests\bin\Debug\netcoreapp3.1\MPT.Math.UnitTests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
+.\Math\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:".\Math\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe" -targetargs:"/nologo /noshadow .\Math\MPT.Math.UnitTests\bin\Debug\netcoreapp2.1\MPT.Math.UnitTests.dll" -filter:"+[*]* -[*.Tests]*" -register:user
  
 .\Math\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
