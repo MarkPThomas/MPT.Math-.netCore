060e83f752c2890fbd3b8387821bfa4217ae2124
Mark P Thomas <markums@gmail.com>
2020-05-21 17:38:14 -0600

    Update .after_test.cmd

--- .after_test.cmd
+++ .after_test.cmd
@@ -1,3 +1,6 @@
 nuget install OpenCover -OutputDirectory packages -Version 4.6.519
+nuget install coveralls.net -OutputDirectory packages -Version 0.412.0
 
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:dotnet.exe -targetargs:"test "".\MPT.Math.UnitTests\MPT.Math.UnitTests.csproj"" -appveyor" -filter:"+[MPT.Math*]*" -oldStyle -output:opencoverCoverage.xml
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:dotnet.exe -targetargs:"test "".\MPT.Math.UnitTests\MPT.Math.UnitTests.csproj""" -filter:"+[MPT.Math*]*" -oldStyle -output:opencoverCoverage.xml
+
+.\packages\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\opencoverCoverage.xml
